cf095a34aaf7c4fe92d488d4d92c373c132eac56
Matte <51192395+MatteZ02@users.noreply.github.com>
2021-04-07 20:58:25 +0300

    fix typings again

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,19 @@
-import Debux from "../src/index";
+import { constructOptions, Options } from "../src/index";
+export * from "../src/index";
 
 declare module "debux" {
-    export default Debux;
+    export default class Debux {
+        constructor(options: constructOptions);
+        private maxCacheSize: number;
+        private cache: string[];
+        private logLevel: number;
+        public log(s: string, options?: Options): void;
+        public error(s: string, options?: Options): void;
+        public warn(s: string, options?: Options): void;
+        public info(s: string, options?: Options): void;
+        public get logs(): string[];
+        private static consoleLog(s: string): void;
+        private constructMessage(s: string, options?: Options): string;
+        private addCache(log: string): void;
+    }
 }
