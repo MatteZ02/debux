9bc40a02c6e07ff90893dfc1767be68c5e732bff
Matte <51192395+MatteZ02@users.noreply.github.com>
2021-06-28 12:59:06 +0300

    fix log entry format

--- src/index.ts
+++ src/index.ts
@@ -45,25 +45,25 @@ export class Debux {
         this.logLevel = options?.logLevel ?? defaultOptions.logLevel;
     }
     public log(s: string | null, options?: Options): void {
-        this.addCache(s ?? "null");
+        this.addCache(this.constructEntry(s, "log", options));
         if (this.logLevel == level.all)
             return Debux.consoleLog(this.constructMessage(s, "log", options));
     }
 
     public error(s: string, options?: Options): void {
-        this.addCache(s);
+        this.addCache(this.constructEntry(s, "error", options));
         if (this.logLevel >= level.error)
             return Debux.consoleLog(this.constructMessage(chalk.red(s), "error", options));
     }
 
     public warn(s: string, options?: Options): void {
-        this.addCache(s);
+        this.addCache(this.constructEntry(s, "warn", options));
         if (this.logLevel >= level.warning)
             return Debux.consoleLog(this.constructMessage(chalk.yellow(s), "warn", options));
     }
 
     public info(s: string, options?: Options): void {
-        this.addCache(s);
+        this.addCache(this.constructEntry(s, "info", options));
         if (this.logLevel >= level.info)
             return Debux.consoleLog(this.constructMessage(s, "info", options));
     }
@@ -83,6 +83,17 @@ export class Debux {
         if (typeof options?.class == "string") msg += chalk.yellow(options.class) + seperator;
         if (typeof options?.event == "string") msg += chalk.green(options.event) + seperator;
         if (typeof options?.function == "string") msg += chalk.blue(options.function) + seperator;
+        if (!s) return msg.slice(0, msg.length - 3);
+        else return msg + s;
+    }
+
+    private constructEntry(s: string | null, cmd: cmd, options?: Options): string {
+        let msg: string =
+            new Date().toUTCString() + seperator + this.getCMDString(cmd);
+        if (typeof options?.process == "string") msg += options.process + seperator;
+        if (typeof options?.class == "string") msg += options.class + seperator;
+        if (typeof options?.event == "string") msg += options.event + seperator;
+        if (typeof options?.function == "string") msg += options.function + seperator;
         if (!s) return msg.slice(0, msg.length - 3);
         else return msg + s;
     }
