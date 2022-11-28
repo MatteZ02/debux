6f98ca6c565fbaac73feec5f635695242fdef9a0
Matte <51192395+MatteZ02@users.noreply.github.com>
2022-11-28 13:54:22 +0200

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,6 @@
-import { constructOptions, Debux } from "../src/index";
+import { ConstructOptions, Debux } from "../src/index";
 export * from "../src/index";
 
 declare module "debux" {
-    export default function(options?: constructOptions): Debux;
+    export default function(options?: ConstructOptions): Debux;
 }
